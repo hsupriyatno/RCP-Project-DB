5c644a3809b5cf734e1a2f540eb9e5dd3bf1c23a
hsupriyatno <hsupriyatno@yahoo.com>
2026-03-10 12:52:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,25 +6,7 @@ st.set_page_config(page_title="Reliability DHC6-300", layout="wide")
 
 st.title("✈️ Reliability Dashboard DHC6-300")
 
-@st.cache_data
-def load_data(file_name, sheet_name):
-    # 1. Membaca Excel
-    df = pd.read_excel(file_name, sheet_name=sheet_name)
-    
-    # 2. Hapus baris & kolom yang 100% kosong
-    df = df.dropna(how='all', axis=0).dropna(how='all', axis=1)
-    
-    # 3. MENGHILANGKAN tulisan 'Col_' atau 'Unnamed'
-    # Kita ganti namanya menjadi spasi kosong agar rapi di tabel
-    new_columns = []
-    for col in df.columns:
-        if "Unnamed" in str(col) or "Col_" in str(col):
-            new_columns.append("") # Jadi kosong/blank
-        else:
-            new_columns.append(col)
-    df.columns = new_columns
-    
-    return df
+
 try:
     # Nama file harus sama persis dengan yang ada di GitHub
     file_target = 'COMPONENT_RELIABILITY_DHC6-300.xlsm'
@@ -49,6 +31,7 @@ try:
 except Exception as e:
     st.error(f"Terjadi kesalahan: {e}")
     st.info("Pastikan file 'COMPONENT_RELIABILITY_DHC6-300.xlsm' sudah di-upload ke GitHub.")
+
 
 
 
